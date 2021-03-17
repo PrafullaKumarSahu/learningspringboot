cc58b4089b01fd7274f3771bdd703409acb1e894
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2021-03-17 11:11:33 +0530

    Implement repository design pattern

--- student/StudentService.java
+++ student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.learningspringboot.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,15 +9,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+    
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2020, 01, 02),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
